c21d7e137072b510303ff912decbb006709f937e
expDARE <5340947+ExtraPotions@users.noreply.github.com>
2026-09-25 15:29:51 -0700

    Pin shared update metadata endpoint

--- src/updates.js
+++ src/updates.js
@@ -1,6 +1,7 @@
 EXP.Updates = ExtraPotionsCore.createReleaseUpdateChecker({
   productId: 'ward',
   repository: 'ExtraPotions/WARD',
+  endpoint: 'https://api.github.com/repos/ExtraPotions/WARD/releases/latest',
   currentVersion: EXP.VERSION,
   enabled: () => EXP.Settings.snapshot().updateNotifications,
   onError: error => EXP.Core.safeError(Object.assign(error, { code: 'UPDATE_CHECK' }), 'ward.updates'),
