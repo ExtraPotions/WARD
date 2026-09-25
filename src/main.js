4c846964e20ca969f7bd0b07c130be2a6cd4cefe
expDARE <5340947+ExtraPotions@users.noreply.github.com>
2026-09-25 15:17:17 -0700

    Prepare WARD 3.2.13

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-EXP.VERSION = '3.2.12';
+EXP.VERSION = '3.2.13';
 ExtraPotionsCore.registerDiagnosticsProduct('ward', EXP.VERSION);
 EXP.App = (() => {
   let scheduler, navigationCleanup, settingsCleanup, lifecycle;
